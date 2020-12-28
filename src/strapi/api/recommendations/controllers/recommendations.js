80fca3bdc3bc1375c941effa73ba058c9b894b3d
Samuel Pitoňák <sampittko@gmail.com>
2020-12-28 16:44:31 +0100

    Random recommendations

--- src/strapi/api/recommendations/controllers/recommendations.js
+++ src/strapi/api/recommendations/controllers/recommendations.js
@@ -1,8 +1,31 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+const { sanitizeEntity } = require("strapi-utils");
+
+module.exports = {
+  /**
+   * Retrieve a record.
+   *
+   * @return {Object}
+   */
+
+  async findOne(ctx) {
+    const { id } = ctx.params;
+
+    if (id !== "random") {
+      return ctx.badRequest("Query parameter is invalid");
+    }
+
+    const recommendations = await strapi.query("recommendations").find();
+
+    const entity =
+      recommendations[Math.floor(Math.random() * recommendations.length)];
+
+    return sanitizeEntity(entity, { model: strapi.models.recommendations });
+  },
+};
