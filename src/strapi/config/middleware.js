e29704cf83814a65d6acdbc8f72b6398d253fc39
Samuel Pitoňák <sampittko@gmail.com>
2020-12-29 23:21:25 +0100

    Update origins

--- src/strapi/config/middleware.js
+++ src/strapi/config/middleware.js
@@ -5,12 +5,14 @@ module.exports = {
       origin:
         process.env.ON_VPS === 1
           ? [
-              process.env.CORS_ORIGIN_NEXT,
+              process.env.CORS_ORIGIN_NEXT_1,
+              process.env.CORS_ORIGIN_NEXT_2,
               process.env.CORS_ORIGIN_STRAPI,
               process.env.CORS_ORIGIN_DOCKER_UPDATER,
             ]
           : [
-              process.env.CORS_ORIGIN_NEXT,
+              process.env.CORS_ORIGIN_NEXT_1,
+              process.env.CORS_ORIGIN_NEXT_2,
               process.env.CORS_ORIGIN_NEXT_DEV,
               process.env.CORS_ORIGIN_STRAPI,
               process.env.CORS_ORIGIN_STRAPI_DEV,
