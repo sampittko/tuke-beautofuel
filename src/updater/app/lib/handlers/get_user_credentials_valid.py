00e2cae6ff89c40348c966288b9c8f0e5cba430b
Samuel Pitoňák <sampittko@gmail.com>
2021-01-21 18:43:15 +0100

    Better loggin but cannot display data in grafana?

--- src/updater/app/lib/handlers/get_user_credentials_valid.py
+++ src/updater/app/lib/handlers/get_user_credentials_valid.py
@@ -3,4 +3,6 @@ from ..api.envirocar import get_envirocar_user
 
 async def handler(x_user, x_token):
     data = await get_envirocar_user(x_user, x_token)
-    return {'valid': False if not 'name' in data and not 'email' in data else True}
+    valid = False if not 'name' in data and not 'email' in data else True
+    print('Credentials of user with name {} validated'.format(x_user))
+    return {'valid': valid}
--- src/updater/app/lib/handlers/post_new_tracks.py
+++ src/updater/app/lib/handlers/post_new_tracks.py
@@ -7,68 +7,116 @@ async def handler(data, x_user, x_token, influxdb_client):
     allTracks = await get_envirocar_tracks(x_user, x_token)
 
     if len(allTracks) == 0:
+        print("There are no tracks to synchronize for user with name {}".format(x_user))
         return {'statusCode': 200, 'message': 'You need to record some driving data first to synchronize them later'}
+    print("Tracks fetched")
 
     existingTracks = await get_strapi_tracks(data.user)
 
     newTracks = filter_tracks(allTracks, existingTracks)
+    newTracksCount = len(newTracks)
 
-    if len(newTracks) == 0:
-        return {'statusCode': 200, 'message': 'There are no new track records'}
+    if newTracksCount == 0:
+        message = 'There are no new track records for user with name {}'.format(
+            x_user)
+        print(message)
+        return {'statusCode': 200, 'message': message}
+    print("There are {} new track records for user with name {}".format(
+        newTracksCount, x_user))
 
-    tracks = []
-    for newTrack in newTracks:
-        completeNewTrack = await get_envirocar_track(x_user, x_token, newTrack['id'])
-
-        userData = completeNewTrack['properties']['user']
-        carData = completeNewTrack['properties']['sensor']['properties']
-
-        tracks.append({
-            'measurement': 'tracks',
-            'tags': {
-                'id': completeNewTrack['properties']['id'],
-                'user': userData['name'],
-                'email': userData['mail'],
-            },
-            'time': completeNewTrack['properties']['created'],
-            'fields': {
-                'length': completeNewTrack['properties']['length'],
-                'car': '{} {} {}'.format(
-                    carData['manufacturer'], carData['model'], carData['constructionYear']),
-                'carEngineDisplacement': carData['engineDisplacement'],
-            }
-        })
-
-        trackFeatures = []
-        for feature in completeNewTrack['features']:
-
-            coordinatesData = feature['geometry']['coordinates']
-            phenomenonsData = feature['properties']['phenomenons']
-
-            if not 'Consumption (GPS-based)' in phenomenonsData or not 'GPS Speed' in phenomenonsData:
-                continue
-
-            trackFeatures.append({
-                'measurement': 'trackFeatures',
-                'tags': {
-                    'id': feature['properties']['id'],
-                    'track': completeNewTrack['properties']['id'],
-                },
-                'time': feature['properties']['time'],
-                'fields': {
-                    'lat': coordinatesData[1],
-                    'lng': coordinatesData[0],
-                    'consumption': phenomenonsData['Consumption (GPS-based)']['value'],
-                    'speed': phenomenonsData['GPS Speed']['value'],
-                }
-            })
-
-        influxdb_client.write_points(trackFeatures)
-    influxdb_client.write_points(tracks)
+    try:
+        await persist_new_tracks_data(newTracks, x_user, x_token, influxdb_client)
+        print("Data persistence completed")
+    except ChildProcessError:
+        print("Data persistence is incomplete")
 
     if data.phaseNumber == 2 or data.phaseNumber == 3:
         print("Pipeline not yet implemented")
 
     # await update_strapi_tracks(newTracks, data.user, data.synchronization, data.phaseNumber, data.userGroup)
 
-    return {'statusCode': 200, 'message': f'{len(newTracks)} tracks were processed'}
+    return {'statusCode': 200, 'message': f'{newTracksCount} tracks were processed'}
+
+
+async def persist_new_tracks_data(newTracks, x_user, x_token, influxdb_client):
+    tracks = []
+    for newTrack in newTracks:
+        completeNewTrack = await get_envirocar_track(x_user, x_token, newTrack['id'])
+
+        trackPoint = build_track_point(completeNewTrack)
+        tracks.append(trackPoint)
+
+        trackFeatures = []
+        for feature in completeNewTrack['features']:
+            try:
+                trackFeaturePoint = build_track_feature_point(
+                    completeNewTrack, feature)
+                trackFeatures.append(trackFeaturePoint)
+            except ValueError as e:
+                print(e.message)
+
+        persistedTrackFeatures = influxdb_client.write_points(trackFeatures)
+        if not persistedTrackFeatures:
+            try:
+                raise ChildProcessError()
+            except ChildProcessError as e:
+                e.message = "Track features persistence was not successful for track".format(
+                    newTrack['id'])
+                raise
+
+    persistedTracks = influxdb_client.write_points(tracks)
+    if not persistedTracks:
+        try:
+            raise ChildProcessError()
+        except ChildProcessError as e:
+            e.message = "Tracks persistence was not successful"
+            raise
+
+
+def build_track_point(completeNewTrack):
+    userData = completeNewTrack['properties']['user']
+    carData = completeNewTrack['properties']['sensor']['properties']
+
+    return {
+        'measurement': 'tracks',
+        'tags': {
+            'id': completeNewTrack['properties']['id'],
+            'user': userData['name'],
+            'email': userData['mail'],
+        },
+        'time': completeNewTrack['properties']['created'],
+        'fields': {
+            'length': completeNewTrack['properties']['length'],
+            'car': '{} {} {}'.format(
+                carData['manufacturer'], carData['model'], carData['constructionYear']),
+            'carEngineDisplacement': carData['engineDisplacement'],
+        }
+    }
+
+
+def build_track_feature_point(completeNewTrack, feature):
+    coordinatesData = feature['geometry']['coordinates']
+    phenomenonsData = feature['properties']['phenomenons']
+
+    if not 'Consumption (GPS-based)' in phenomenonsData or not 'GPS Speed' in phenomenonsData:
+        try:
+            raise ValueError()
+        except ValueError as e:
+            e.message = "Track feature point ID {} has missing data inside track ID {}".format(
+                feature['properties']['id'], completeNewTrack['properties']['id'])
+            raise
+
+    return {
+        'measurement': 'trackFeatures',
+        'tags': {
+            'id': feature['properties']['id'],
+            'track': completeNewTrack['properties']['id'],
+        },
+        'time': feature['properties']['time'],
+        'fields': {
+            'lat': coordinatesData[1],
+            'lng': coordinatesData[0],
+            'consumption': phenomenonsData['Consumption (GPS-based)']['value'],
+            'speed': phenomenonsData['GPS Speed']['value'],
+        }
+    }
